bfc1f453691d97e92047e1c099ca18753f56879c
Angie Yang <angieyang@Angies-MacBook-Pro.local>
2016-10-21 17:59:38 -0400

    hellow world

---
+++ ByteShortInt/src/Main.java
@@ -0,0 +1,7 @@
+/**
+ * Created by angieyang on 10/19/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ HelloWorld/src/hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by angieyang on 10/19/16.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Angie!");
+        int myFirstNumber = 9;
+        System.out.println(myFirstNumber);
+    }
+}
